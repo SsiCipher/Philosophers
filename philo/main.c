fa421a7f2a221815044f8b9c52870b6d64dbf07d
SsiCipher <ssicipher@mail.com>
2022-06-02 23:28:12 +0100

    🍝 ADDED: meals watch

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 11:10:07 by yanab             #+#    #+#             */
-/*   Updated: 2022/05/24 15:39:09 by yanab            ###   ########.fr       */
+/*   Updated: 2022/06/02 23:27:43 by yanab            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,22 +65,28 @@ int	monitor_death(t_data *data)
 int	monitor_meals_count(t_data *data)
 {
 	int	i;
+	int philos_done_eating;
 
 	i = 0;
-	while (
-		i < data->philos_count
-		&& data->philos[i].n_times_eaten <= data->n_times_to_eat
-	)
+	philos_done_eating = 0;
+	while (i < data->philos_count)
+	{
+		if (data->philos[i].n_times_eaten >= data->n_times_to_eat)
+			philos_done_eating++;
 		i++;
-	if (i == data->philos_count)
+	}
+
+	if (philos_done_eating == data->philos_count)
+	{
+		printf("All philosophers have eaten %d times\n", data->n_times_to_eat);
 		return (1);
+	}
 	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	t_data		*data;
-	// pthread_t	eat_times_thread;
 
 	data = (t_data *)malloc(sizeof(t_data));
 	if (argc < 5 || argc > 6)
@@ -88,14 +94,12 @@ int	main(int argc, char **argv)
 	if (!init_data(data, argc - 1, argv + 1))
 		return (print_error("Error:\nFailed to initialize data\n"));
 	start_philos(data);
-	// if (data->n_times_to_eat != -1)
-	// {
-	// 	pthread_create(&eat_times_thread, NULL, monitor_meals_count, NULL);
-	// 	pthread_detach(eat_times_thread);
-	// }
 	while (1)
 	{
-		if (monitor_death(data))
+		if (
+			monitor_death(data)
+			|| (data->n_times_to_eat != -1 && monitor_meals_count(data))
+		)
 			return (0);
 	}
 	return (0);
